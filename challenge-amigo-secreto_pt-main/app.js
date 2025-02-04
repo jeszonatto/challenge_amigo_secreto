2bec11f29672c271aea522fd3850728b126ee5cd
Jessica Zonatto <jessica.zonatto1@gmail.com>
2025-02-04 01:03:32 -0300

    finalizado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeNomes = []
+
+function adicionarNome() {
+    let nome = document.querySelector('#amigo').value;
+    //console.log ('campo digitado: ', nome);
+   if (nome == '') {
+        alert('Por favor, digite um nome valido.');
+    } else {
+        listaDeNomes.push(nome)
+        //console.log('lista: ', listaDeNomes )
+        const lista = document.querySelector('#listaAmigos');
+        lista.innerHTML = ''
+        for (let i = 0; i < listaDeNomes.length; i++) {
+            lista.innerHTML += `<li>${listaDeNomes[i]}</li>`
+        }
+    }
+    document.querySelector('#amigo').value = '';
+}
+
+
+

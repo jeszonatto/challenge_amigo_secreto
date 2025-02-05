447944997c1f793f6dfdc881a02957fe7c9676ae
Jessica Zonatto <jessica.zonatto1@gmail.com>
2025-02-04 23:06:35 -0300

    organizando pasta

--- app.js
+++ app.js
@@ -8,16 +8,30 @@ function adicionarNome() {
    if (nome == '') {
         alert('Por favor, digite um nome valido.');
     } else {
-        listaDeNomes.push(nome)
+        document.getElementById('resultado').innerHTML = '';
+        listaDeNomes.push(nome);
         //console.log('lista: ', listaDeNomes )
         const lista = document.querySelector('#listaAmigos');
         lista.innerHTML = ''
         for (let i = 0; i < listaDeNomes.length; i++) {
-            lista.innerHTML += `<li>${listaDeNomes[i]}</li>`
+            lista.innerHTML += `<li>${listaDeNomes[i]}</li>`;
         }
     }
     document.querySelector('#amigo').value = '';
 }
 
+function sortearAmigo() {
+    //console.log('o nome foi sorteado');
+    if (listaDeNomes.length !== 0) {
+        const nome = sortearUmNome();
+        const resultado = document.getElementById('resultado');
+        resultado.innerHTML += `<li>${nome}</li>`;
+        listaDeNomes = []
+        document.querySelector('#listaAmigos').innerHTML = '';
+    }   
+}
 
+function sortearUmNome() {
+    return listaDeNomes[Math.floor(Math.random()* listaDeNomes.length)]; 
+}
 
